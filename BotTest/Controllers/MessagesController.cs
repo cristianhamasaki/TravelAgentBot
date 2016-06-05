8ad3d192b6945afd2eff1401aef39ac9f60e317c
rui.deng <rui.deng365@outlook.com>
2016-06-05 13:10:11 +0200

    Hello word bot framework application with LUIS
    It contains TravelAgentDialog that implements LuisDiag and binded to
    LUIS application TravelAgnent.

---
+++ BotTest/Controllers/MessagesController.cs
@@ -0,0 +1,66 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace BotTest
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<Message> Post([FromBody]Message message)
+        {
+            if (message.Type == "Message")
+            {
+                //// calculate something for us to return
+                //int length = (message.Text ?? string.Empty).Length;
+
+                //// return our reply to the user
+                //return message.CreateReplyMessage($"You just said {length} characters");
+
+                // return our reply to the user
+                return await Conversation.SendAsync(message, () => new TravelAgentDialog());
+            }
+            else
+            {
+                return HandleSystemMessage(message);
+            }
+        }
+
+        private Message HandleSystemMessage(Message message)
+        {
+            if (message.Type == "Ping")
+            {
+                Message reply = message.CreateReplyMessage();
+                reply.Type = "Ping";
+                return reply;
+            }
+            else if (message.Type == "DeleteUserData")
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == "BotAddedToConversation")
+            {
+            }
+            else if (message.Type == "BotRemovedFromConversation")
+            {
+            }
+            else if (message.Type == "UserAddedToConversation")
+            {
+            }
+            else if (message.Type == "UserRemovedFromConversation")
+            {
+            }
+            else if (message.Type == "EndOfConversation")
+            {
+            }
+
+            return null;
+        }
+    }
+}
---
+++ BotTest/TravelAgentDialog.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+namespace BotTest
+{
+    [LuisModel("711a5176-f87a-473e-9e25-856b475f21eb", "30947615e9b04edea0c30a91b7ee1135")]
+    [Serializable]
+    public class TravelAgentDialog : LuisDialog<object>
+    {
+        public const string Entity_Location_ToLocation = "Location::ToLocation";
+
+        public TravelAgentDialog(ILuisService service = null)
+            : base(service)
+        {
+        }
+
+        [LuisIntent("BookFlight")]
+        public async Task BookFlight(IDialogContext context, LuisResult result)
+        {
+            string where = string.Empty;
+
+            EntityRecommendation toLocation;
+            if (result.TryFindEntity(Entity_Location_ToLocation, out toLocation))
+            {
+                where = toLocation.Entity;
+            }
+
+            if (!string.IsNullOrEmpty(where))
+            {
+                await context.PostAsync($"Ok, flight to {where} has been booked successfully.");
+            }
+            else
+            {
+                await context.PostAsync("Please specify your destination.");
+            }
+
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            string message = $"Sorry I did not understand what you said.";
+
+            await context.PostAsync(message);
+            context.Wait(MessageReceived);
+        }
+
+    }
+}
